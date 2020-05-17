bf082a6e2dabc4bbb7edc03f0106da929d00c2f3
Fela Ameghino <frayx@live.it>
2020-05-17 18:27:43 +0200

    Media sender progress

--- Unigram/Unigram/ViewModels/DialogViewModel.Media.cs
+++ Unigram/Unigram/ViewModels/DialogViewModel.Media.cs
@@ -1,7 +1,6 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 using Telegram.Td;
@@ -261,7 +260,7 @@ namespace Unigram.ViewModels
                 caption = formattedText.Substring(0, CacheService.Options.MessageCaptionLengthMax);
             }
 
-            var dialog = new SendFilesView(items, media);
+            var dialog = new SendFilesView(items, media, _chat.Type is ChatTypePrivate);
             dialog.ViewModel = this;
             dialog.Caption = caption;
 
@@ -904,8 +903,6 @@ namespace Unigram.ViewModels
 
         public async Task HandlePackageAsync(DataPackageView package)
         {
-            var boh = string.Join(", ", package.AvailableFormats);
-
             if (package.AvailableFormats.Contains(StandardDataFormats.Bitmap))
             {
                 var bitmap = await package.GetBitmapAsync();
@@ -1098,7 +1095,7 @@ namespace Unigram.ViewModels
 
             var formattedText = GetFormattedText(true);
 
-            var dialog = new SendFilesView(new List<StorageMedia> { storage }, true);
+            var dialog = new SendFilesView(new[] { storage }, true, false);
             dialog.Caption = formattedText
                 .Substring(0, CacheService.Options.MessageCaptionLengthMax);
 
