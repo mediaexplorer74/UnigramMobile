8b2106d676df2eb466446fe8476d6dfafd35c872
Fela Ameghino <frayx@live.it>
2020-06-07 13:47:41 +0200

    Removed unwanted code

--- Unigram/Unigram/Views/ChatView.xaml.cs
+++ Unigram/Unigram/Views/ChatView.xaml.cs
@@ -56,7 +56,6 @@ namespace Unigram.Views
 
         private Func<IDialogDelegate, DialogViewModel> _getViewModel;
         private DialogViewModel _viewModel;
-        private double _lastKnownKeyboardHeight = 260;
 
         private readonly TLWindowContext _windowContext;
 
@@ -827,8 +826,6 @@ namespace Unigram.Views
             StickersPanel.Height = args.OccludedRect.Height;
             ReplyMarkupPanel.MaxHeight = args.OccludedRect.Height;
             //ReplyMarkupViewer.MaxHeight = args.OccludedRect.Height;
-
-            _lastKnownKeyboardHeight = Math.Max(260, args.OccludedRect.Height);
 
             Collapse_Click(null, null);
             CollapseMarkup(false);
@@ -2870,7 +2867,6 @@ namespace Unigram.Views
                 _stickersMode = StickersPanelMode.Collapsed;
                 SettingsService.Current.IsSidebarOpen = false;
 
-                StickersPanel.MinHeight = 260;
                 StickersPanel.MaxHeight = 360;
                 StickersPanel.Height = _lastKnownKeyboardHeight;
                 ButtonExpand.Glyph = "\uE010";
