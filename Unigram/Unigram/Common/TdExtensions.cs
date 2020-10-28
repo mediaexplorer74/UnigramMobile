775231b71ca244b71f994b5bbcb6f0d674b7d4a3
Fela Ameghino <frayx@live.it>
2020-10-28 11:01:36 +0100

    Added service message

--- Unigram/Unigram/Common/TdExtensions.cs
+++ Unigram/Unigram/Common/TdExtensions.cs
@@ -1045,6 +1045,7 @@ namespace Unigram.Common
                 case MessageContactRegistered contactRegistered:
                 case MessageCustomServiceAction customServiceAction:
                 case MessageGameScore gameScore:
+                case MessageLiveLocationApproached liveLocationApproached:
                 case MessagePassportDataSent passportDataSent:
                 case MessagePaymentSuccessful paymentSuccessful:
                 case MessagePinMessage pinMessage:
